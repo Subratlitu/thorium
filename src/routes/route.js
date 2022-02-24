8f08079021de2cee0364982726c354d4bc0a6e20
subratlitu <subratnayak940@gmail.com>
2022-02-24 10:14:45 +0530

    adding problem number1&2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,78 +1,114 @@
 const express = require('express');
 const router = express.Router();
-//1
-router.get('/movies', function(req, res) {
-    
-    res.send('["dhoom 2","krish","war","robort","bahubali","kgf"]')
-})
 
+// //1
+// router.get('/sol1', function(req, res) {
+//     let arr=[1,2,3,5,6,7]
+//     let misNumber
+//    let sum=7*(7+1)/2
+//    let arrSum=0
+//    for(let i=0;i<arr.length;i++){
+//        arrSum=arrSum+arr[i];
+
+//    }
+//    misNumber=sum-arrSum;
+    
+//     res.send({data:misNumber})
+        
+// })
 //2
-router.get('/movies/:movieId', function(req, res) {
-    mov=["dhoom 2","krish","war","robort","bahubali","kgf"]
-    let ind=req.params.movieId
-    if (ind>mov.length-1) {
-        res.send("movies index does not exit")
-    }
-    else{
-        res.send(mov[ind])
-    }
+// router.get('/sol2', function(req, res) {
+//     let arr1=[33, 34, 35, 37, 38]
+//     let missNumber
+//    let sum1=6*(33+38)/2
+//    let arr1Sum=0
+//    for(let i=0;i<arr1.length;i++){
+//        arr1Sum=arr1Sum+arr1[i];
+
+//    }
+//    missNumber=sum1-arr1Sum;
     
-})
-//3
-router.get('/film', function(req, res) {
-    
-    res.send('[ {“id”: 1,“name”: “The Shining”},{“id”: 2,“name”: “titanic”},{“id”: 3,“name”: “inception”},{“id”: 4,“name”: “avtar”}]')
+//     res.send({data:missNumber})
         
-})
-//4
-router.get('/film/:filmId', function(req, res) {
-    
-    const movi= [{'id':1,'name':'the shining'},{'id':2,'name':'titanic'},{'id':3,'name':'inception'},{'id':4,'name':'avtar'}]
-    let val=req.params.filmId;
-    let flag=false
-    for(let i=0;i<movi.length;i++){
-        if(movi[i].id==val){
-            flag=true
-            res.send(movi[i])
+// })
+
+
+let player=[ {
+    "name": "manish",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+      "swimming"
+    ],
+    "bookings": []
+  },{
+    "name": "rahul",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+      "swimming"
+    ],
+    "bookings": []
+  },{
+    "name": "virat",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+      "swimming"
+    ],
+    "bookings": []
+  },{
+    "name": "rohit",
+    "dob": "1/1/1995",
+    "gender": "male",
+    "city": "jalandhar",
+    "sports": [
+      "swimming"
+    ],
+    "bookings": []
+ }]
+ 
+    //res.send(  { msg: arr , status: true }  )
+    //console.log(player[0].name)
+ 
+
+    router.post("/play/:name/:dob/:gender/:city/:sports/:bookings", function(req, res){
+
+      let userName=req.params.name
+      let userDob=req.params.dob
+      let userGender=req.params.gender
+      let userCity=req.params.city
+      let userSports=req.params.sports
+      let userBookings=req.params.bookings
+
+      let newPlayer={}
+
+      newPlayer.name=userName
+      newPlayer.dob=userDob
+      newPlayer.gender=userGender
+      newPlayer.city=userCity
+      newPlayer.sports=userSports
+      newPlayer.bookings=userBookings
+      let mark=0
+
+      for (let i=0;i<player.length;i++){
+        if (player[i].name===userName){
+            res.send("player already exist!")
+            mark=1
             break;
-        }
-        else{
-            res.send("no movie available for this id")
-        }
+        } 
     }
-        
-})
-//5
-router.get('/sol1', function(req, res) {
-    let arr=[1,2,3,5,6,7]
-    let misNumber
-   let sum=7*(7+1)/2
-   let arrSum=0
-   for(let i=0;i<arr.length;i++){
-       arrSum=arrSum+arr[i];
+    if (mark===0) {
+      player.push(newPlayer)
+    }
+    player.push(newPlayer)
+    res.send(player)
+    })    
 
-   }
-   misNumber=sum-arrSum;
-    
-    res.send({data:misNumber})
-        
-})
-//6
-router.get('/sol2', function(req, res) {
-    let arr1=[33, 34, 35, 37, 38]
-    let missNumber
-   let sum1=6*(33+38)/2
-   let arr1Sum=0
-   for(let i=0;i<arr1.length;i++){
-       arr1Sum=arr1Sum+arr1[i];
-
-   }
-   missNumber=sum1-arr1Sum;
-    
-    res.send({data:missNumber})
-        
-})
-
+//console.log(player)
 
 
 module.exports = router;
